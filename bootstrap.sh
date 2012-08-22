0a869ebe22d904a7739d1e7d32370a93169c578a
Nicolae Vartolomei <nvartolomei@gmail.com>
2012-08-22 19:03:57 +0300

    bootstrap.sh: Save new backup every time

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,16 @@
 cd "$(dirname "$0")"
 #git pull
 
+now=$(date +%s)
+
 function do_backup() {
     shopt -s dotglob
 
-    ! [ -d "$HOME/.backup" ] && mkdir $HOME/.backup
+    mkdir -p "$HOME/.backups/$now"
 
     for file in *; do
-        [ -f "$HOME/$file" ] && cp -r "$HOME/$file" "$HOME/.backup/$file"
+        [ -f "$HOME/$file" ] && cp -r "$HOME/$file" "$HOME/.backups/$now/$file"
     done
-
-    echo "Your original dotfiles has backed up to ~/.backup/"
 }
 
 function do_sync() {
@@ -23,15 +23,20 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
     do_sync
     do_backup
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo "This may overwrite existing files in your home directory,"
+    echo "a backup will be saved in \`~/.backups/$now\`."
+
+    read -p "Are you sure? (y/n) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         do_sync
         do_backup
     fi
 fi
+
 unset do_sync
 unset do_backup
+unset now
 
 # Check what homebrew is saying
 # source ~/.brew
